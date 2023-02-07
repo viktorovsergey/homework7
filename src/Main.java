346f612bf0edf973b57afe1fcedb3ac3fc498038
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-02-07 21:16:35 +0300

    все задачи решены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,115 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("------Задача 1------");
+        task1();
+        System.out.println();
+        System.out.println("------Задача 2------");
+        task2();
+        System.out.println("");
+        System.out.println("------Задача 3------");
+        task3();
+        System.out.println("");
+        System.out.println("------Задача 4------");
+        task4();
+        System.out.println("");
+        System.out.println("------Задача 5------");
+        task5();
+        System.out.println("");
+        System.out.println("------Задача 6------");
+        task6();
+        System.out.println("");
+        System.out.println("------Задача 7------");
+        task7();
+        System.out.println("");
+        System.out.println("------Задача 8------");
+        task8();
+        System.out.println("");
+        System.out.println("------Задача 9------");
+        task9();
+        System.out.println("");
+        System.out.println("------Задача 10------");
+        task10();
+        System.out.println("");
     }
+
+
+    public static void task1() {
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Номер итерации " + i);
+        }
+    }
+
+    public static void task2() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("обратная итерации " + i);
+        }
+    }
+
+    public static void task3() {
+        for (int i = 0; i < 17; i = i + 2) {
+            if (i % 2 == 0 && i > 0) {
+                System.out.println("Из задданых чисел от 1 до 17 четное число " + i);
+            }
+        }
+    }
+
+    public static void task4() {
+        for (int i = 10; i > -11; i--) {
+            System.out.println("Обратный счет " + i);
+        }
+    }
+
+    public static void task5() {
+        for (int i = 1904; i < 2097; i = i + 4) {
+            System.out.println(i + " год является високостным ");
+        }
+    }
+
+    public static void task6() {
+        int factor = 7;
+        for (int i = 1; i < 15; i++) {
+            System.out.print(i * factor + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void task7() {
+        int factor = 2;
+        int result = 1;
+        for (int i = 1; i < 11; i++) {
+            System.out.print(result + " ");
+            result = result * factor;
+        }
+        System.out.println();
+    }
+
+    public static void task8() {
+        int moneyInYear = 29000;
+        int totalMoney=0;
+        for (int i = 1; i < 13; i++) {
+            totalMoney=totalMoney+moneyInYear;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + totalMoney + " рублей");
+        }
+    }
+    public static void task9() {
+        int moneyInYear = 29000;
+        int totalMoney=0;
+        for (int i = 1; i < 13; i++) {
+            totalMoney=totalMoney+totalMoney/100;
+            totalMoney=totalMoney+moneyInYear;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + totalMoney + " рублей");
+        }
+    }
+    public static void task10() {
+        int factor = 2;
+        for (int i = 1; i < 11; i = ++i) {
+            System.out.println(i*factor);
+        }
+    }
+
 }
+
+
